024c6c161a3717d51218f517244274b08b7abba6
ChristianAbraham <christian.abraham.putra-2021@fst.unair.ac.id>
2023-02-14 13:24:04 +0700

    layouting home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,161 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main(List<String> args) {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          toolbarHeight: 120,
+          elevation: 0,
+          backgroundColor: Colors.white,
+          title: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                      child: Text("Hello!", 
+                      style: TextStyle(color: Colors.black, 
+                      fontSize: 20,
+                      fontFamily: "Montserrat", 
+                      fontWeight: FontWeight.w700)),
+                    ),
+                    Text("Chris", style: TextStyle(color: Colors.orange, 
+                      fontSize: 32,
+                      fontFamily: "Montserrat",
+                      fontWeight: FontWeight.w700)),
+                    
+                  ],
+                ),
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                  child: CircleAvatar(
+                    radius: 30,
+                    backgroundImage: NetworkImage(""),
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          )
+
+          
+          ),
+        body: 
+        Padding(
+          padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+          child: Column(
+            
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(bottom: 20, top: 10),
+                child: Text("Main Article", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, fontFamily: "Montserrat")),
+              ),
+              SizedBox(height: 160,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 20),
+                    child: Container(
+                      width: 280,
+                      height: 160,
+                      color: Colors.red,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 20),
+                    child: Container(
+                      width: 280,
+                      height: 160,
+                      color: Colors.orange,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 20),
+                    child: Container(
+                      width: 280,
+                      height: 160,
+                      color: Colors.yellow,
+                    ),
+                  ),
+                  
+                ],
+              ),
+            ),
+              const Padding(
+                padding: EdgeInsets.only(bottom: 10, top: 20),
+                child: Text("Daily Insight", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, fontFamily: "Montserrat")),
+              ),
+              Flexible(child: 
+              ListView(
+                  scrollDirection: Axis.vertical,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 20),
+                      child: Container(
+                        width: 480,
+                        height: 160,
+                        color: Colors.green,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 20),
+                      child: Container(
+                        width: 480,
+                        height: 160,
+                        color: Colors.blue,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 20),
+                      child: Container(
+                        width: 480,
+                        height: 160,
+                        color: Colors.purple,
+                      ),
+                    ),
+                  ],
+
+                ))
+              
+            ],),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
             ),
           ],
+          selectedItemColor: Colors.orange,
+          unselectedItemColor: Colors.grey
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
+      )
     );
   }
 }
