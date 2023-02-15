0c2112dde6a8469e969e6e05c6b535c9024c3657
ChristianAbraham <christian.abraham.putra-2021@fst.unair.ac.id>
2023-02-15 15:58:30 +0700

    nambah akeh pokoke lali opo sg tak tambahno yas

---
+++ lib/Pages/article.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class Article extends StatelessWidget {
+  const Article({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        iconTheme: IconThemeData(color: Colors.white, size: 30),
+        
+      ),
+      body: Column(children: [
+        SizedBox(
+          height: 180,
+          width: 480,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: Image.network("https://picsum.photos/280/130", 
+            fit: BoxFit.cover,)
+          )
+        ),
+        const Padding(
+          padding: EdgeInsets.all(15),
+          child: Text("Ini Judul Lorem Ipsum Yang Panjang Ya Sapatau Menarik",
+          style: TextStyle(fontFamily: 'Montserrat', 
+          fontSize: 25, 
+          fontWeight: FontWeight.w700,
+          height: 1.5),
+          ),
+        ),
+        
+      ]),
+    );
+  }
+}
