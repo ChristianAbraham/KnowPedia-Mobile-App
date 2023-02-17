8e14e39d3fadfed0c1e11ba52a9c9875bbe68726
sakti17 <106225961+sakti17@users.noreply.github.com>
2023-02-17 10:08:21 +0700

    ga ro

--- lib/Pages/explore.dart
+++ lib/Pages/explore.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:knowpedia/Pages/homepage.dart';
 
 const Color warnaUngu = Color.fromARGB(255, 75, 38, 72);
 const Color warnaOren = Color.fromARGB(255, 239, 140, 51);
@@ -14,16 +15,62 @@ class Explore extends StatelessWidget {
         iconTheme: const IconThemeData(color: warnaUngu, size: 30),
         centerTitle: true,
         toolbarHeight: 80,
-        title: const Text('Explore', 
-        style: TextStyle(color: warnaUngu,
-        fontFamily: "Montserrat",
-        fontWeight: FontWeight.bold,
-        fontSize: 25)),
+        title: const Text('Explore',
+            style: TextStyle(
+                color: warnaUngu,
+                fontFamily: "Montserrat",
+                fontWeight: FontWeight.bold,
+                fontSize: 25)),
         elevation: 0,
         backgroundColor: Colors.white,
       ),
-      body: const Center(
-        child: Text("Ini Explore"),
+      body: Column(
+        children: [
+          Flexible(
+            child: Container(
+              child: ListView(
+                scrollDirection: Axis.vertical,
+                children: [
+                  Container(width: 306, height: 149, color: warnaOren),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          width: 148,
+                          height: 170,
+                          color: warnaUngu,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          children: [
+                            Container(
+                              width: 148,
+                              height: 78,
+                              color: warnaOren,
+                            ),
+                            Container(
+                              width: 148,
+                              height: 78,
+                              color: warnaUngu,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  MainArticle(),
+                  MainArticle(),
+                  MainArticle(),
+                  MainArticle(),
+                  MainArticle(),
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
--- lib/Pages/homepage.dart
+++ lib/Pages/homepage.dart
@@ -7,13 +7,11 @@ const Color warnaOren = Color.fromARGB(255, 239, 140, 51);
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
-        backgroundColor: Colors.white,
-        appBar: AppBar(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
           toolbarHeight: 95,
           elevation: 0,
           backgroundColor: Colors.white,
@@ -28,17 +26,19 @@ class HomePage extends StatelessWidget {
                   children: const [
                     Padding(
                       padding: EdgeInsets.fromLTRB(0, 0, 0, 3),
-                      child: Text("Hello!", 
-                      style: TextStyle(color: warnaUngu, 
-                      fontSize: 19,
-                      fontFamily: "Montserrat", 
-                      fontWeight: FontWeight.w700)),
+                      child: Text("Hello!",
+                          style: TextStyle(
+                              color: warnaUngu,
+                              fontSize: 19,
+                              fontFamily: "Montserrat",
+                              fontWeight: FontWeight.w700)),
                     ),
-                    Text("Christian", style: TextStyle(color: Colors.orange, 
-                      fontSize: 32,
-                      fontFamily: "Montserrat",
-                      fontWeight: FontWeight.w700)),
-                    
+                    Text("Christian",
+                        style: TextStyle(
+                            color: Colors.orange,
+                            fontSize: 32,
+                            fontFamily: "Montserrat",
+                            fontWeight: FontWeight.w700)),
                   ],
                 ),
                 ElevatedButton(
@@ -47,62 +47,73 @@ class HomePage extends StatelessWidget {
                     shape: const CircleBorder(),
                     elevation: 0,
                   ),
-                  child:const Padding(
+                  child: const Padding(
                     padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
                     child: CircleAvatar(
                       radius: 30,
-                      backgroundImage: NetworkImage("https://picsum.photos/130/130"),
+                      backgroundImage:
+                          NetworkImage("https://picsum.photos/130/130"),
                     ),
                   ),
                   onPressed: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => const Profile()));
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const Profile()));
                   },
                 ),
               ],
             ),
-          )
-          ),
-        body: 
-        Padding(
-          padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Flexible(child: 
-              ListView(
-                  scrollDirection: Axis.vertical,
-                  children: [
-                    const Padding(
-                padding: EdgeInsets.only(bottom: 20, top: 10),
-                child: Text("Main Article", style: TextStyle(color: warnaUngu,fontSize: 20, fontWeight: FontWeight.bold, fontFamily: "Montserrat")),
-              ),
-              SizedBox(height: 180,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: const [
-                  MainArticle(),
-                  MainArticle(),
-                  MainArticle(),
-                  MainArticle(),
-                ],
-              ),
-            ),
+          )),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Flexible(
+                child: ListView(
+              scrollDirection: Axis.vertical,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 20, top: 10),
+                  child: Text("Main Article",
+                      style: TextStyle(
+                          color: warnaUngu,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "Montserrat")),
+                ),
+                SizedBox(
+                  height: 180,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: const [
+                      MainArticle(),
+                      MainArticle(),
+                      MainArticle(),
+                      MainArticle(),
+                    ],
+                  ),
+                ),
                 const Padding(
                   padding: EdgeInsets.only(bottom: 10, top: 20),
-                  child: Text("Daily Insight", style: TextStyle(color: warnaUngu,fontSize: 20, fontWeight: FontWeight.bold, fontFamily: "Montserrat")),
+                  child: Text("Daily Insight",
+                      style: TextStyle(
+                          color: warnaUngu,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "Montserrat")),
                 ),
-                    const DailyInsight(),
-                    const DailyInsight(),
-                    const DailyInsight(),
-                    const DailyInsight(),
-                    const DailyInsight(),
-                    
-                  ],
-
-                ))
-              
-            ],),
+                const DailyInsight(),
+                const DailyInsight(),
+                const DailyInsight(),
+                const DailyInsight(),
+                const DailyInsight(),
+              ],
+            ))
+          ],
         ),
+      ),
     );
   }
 }
@@ -116,7 +127,8 @@ class DailyInsight extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) => const Article()));
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => const Article()));
       },
       child: Padding(
         padding: const EdgeInsets.only(bottom: 20, top: 5),
@@ -129,10 +141,11 @@ class DailyInsight extends StatelessWidget {
             children: [
               ClipRRect(
                 borderRadius: BorderRadius.circular(15),
-                child: const Image(image: NetworkImage("https://picsum.photos/90/100"), 
-                width: 90, 
-                height: 100, 
-                fit: BoxFit.cover,
+                child: const Image(
+                  image: NetworkImage("https://picsum.photos/90/100"),
+                  width: 90,
+                  height: 100,
+                  fit: BoxFit.cover,
                 ),
               ),
               Padding(
@@ -142,32 +155,34 @@ class DailyInsight extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     const Padding(
-                      padding: EdgeInsets.only(top: 0,bottom: 1),
+                      padding: EdgeInsets.only(top: 0, bottom: 1),
                       child: SizedBox(
                         width: 250,
-                        child: Text("Ini Judul Artikel Yang Eye Catchy Tes Judul Panjang",
-                          maxLines: 2,
-                          overflow: TextOverflow.ellipsis, 
-                          style: TextStyle(color: warnaUngu, 
-                          fontSize: 13, 
-                          fontWeight: FontWeight.w700, 
-                          height: 1.5,
-                          fontFamily: "Montserrat")
-                          ),
+                        child: Text(
+                            "Ini Judul Artikel Yang Eye Catchy Tes Judul Panjang",
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                                color: warnaUngu,
+                                fontSize: 13,
+                                fontWeight: FontWeight.w700,
+                                height: 1.5,
+                                fontFamily: "Montserrat")),
                       ),
                     ),
                     const SizedBox(
                       width: 250,
-                      child: Text("Ini subtitle artikel yang menarik tapi ndatau kalian bakal tertarik atau tidak ya memang agak panjang ya",
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis
-                      ,style: TextStyle(color: warnaUngu,
-                      height: 1.5,
-                      fontSize: 9,
-                      fontWeight: FontWeight.w300,
-                      fontFamily: "Montserrat"),
+                      child: Text(
+                        "Ini subtitle artikel yang menarik tapi ndatau kalian bakal tertarik atau tidak ya memang agak panjang ya",
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                            color: warnaUngu,
+                            height: 1.5,
+                            fontSize: 9,
+                            fontWeight: FontWeight.w300,
+                            fontFamily: "Montserrat"),
                       ),
-                    
                     ),
                     Padding(
                       padding: const EdgeInsets.only(top: 6),
@@ -178,30 +193,31 @@ class DailyInsight extends StatelessWidget {
                             width: 50,
                             height: 13,
                             decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(3),
-                              color: warnaUngu
-                            ),
+                                borderRadius: BorderRadius.circular(3),
+                                color: warnaUngu),
                             child: const Center(
-                              child: Text("Science", 
+                                child: Text(
+                              "Science",
                               style: TextStyle(
-                                color: Colors.white, 
-                                fontSize: 10, 
-                                fontWeight: FontWeight.w700, 
-                                fontFamily: "Montserrat"),
-                                )
-                              ),
+                                  color: Colors.white,
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.w700,
+                                  fontFamily: "Montserrat"),
+                            )),
                           ),
                           const Padding(
                             padding: EdgeInsets.only(left: 5),
-                            child: Text("• By Hana", style: TextStyle(color: warnaOren, fontSize: 10, fontWeight: FontWeight.w500, fontFamily: "Montserrat")
-                            ),
+                            child: Text("• By Hana",
+                                style: TextStyle(
+                                    color: warnaOren,
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.w500,
+                                    fontFamily: "Montserrat")),
                           ),
-                          
                         ],
                       ),
                     )
                   ],
-              
                 ),
               )
             ],
@@ -221,7 +237,8 @@ class MainArticle extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
-        Navigator.push(context, MaterialPageRoute(builder: (context) => const Article()));
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => const Article()));
       },
       child: Padding(
         padding: const EdgeInsets.only(right: 35),
@@ -235,23 +252,25 @@ class MainArticle extends StatelessWidget {
             children: [
               ClipRRect(
                 borderRadius: BorderRadius.circular(15),
-                child: const Image(image: NetworkImage("https://picsum.photos/280/130"), 
-                  width: 280, 
-                  height: 130, 
+                child: const Image(
+                  image: NetworkImage("https://picsum.photos/280/130"),
+                  width: 280,
+                  height: 130,
                   fit: BoxFit.cover,
-               ),
-              ),  
+                ),
+              ),
               const Padding(
                 padding: EdgeInsets.fromLTRB(0, 10, 0, 5),
                 child: SizedBox(
                   width: 280,
-                  child: Text("Lorem Ipsum Judul Waw", 
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(color: warnaUngu, 
-                  fontSize: 14, 
-                  fontWeight: FontWeight.w700, 
-                  fontFamily: "Montserrat")),
+                  child: Text("Lorem Ipsum Judul Waw",
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                          color: warnaUngu,
+                          fontSize: 14,
+                          fontWeight: FontWeight.w700,
+                          fontFamily: "Montserrat")),
                 ),
               ),
               Row(
@@ -259,19 +278,33 @@ class MainArticle extends StatelessWidget {
                 children: const [
                   Padding(
                     padding: EdgeInsets.fromLTRB(0, 2, 8, 0),
-                    child: Text("• Science", style: TextStyle(color: warnaOren, fontSize: 10, fontWeight: FontWeight.w500, fontFamily: "Montserrat")),
+                    child: Text("• Science",
+                        style: TextStyle(
+                            color: warnaOren,
+                            fontSize: 10,
+                            fontWeight: FontWeight.w500,
+                            fontFamily: "Montserrat")),
                   ),
                   Padding(
                     padding: EdgeInsets.fromLTRB(0, 2, 8, 0),
-                    child: Text("• By Hana", style: TextStyle(color: warnaOren, fontSize: 10, fontWeight: FontWeight.w500, fontFamily: "Montserrat")),
+                    child: Text("• By Hana",
+                        style: TextStyle(
+                            color: warnaOren,
+                            fontSize: 10,
+                            fontWeight: FontWeight.w500,
+                            fontFamily: "Montserrat")),
                   ),
                   Padding(
                     padding: EdgeInsets.fromLTRB(0, 2, 8, 0),
-                    child: Text("• 2.5k View", style: TextStyle(color: warnaOren, fontSize: 10, fontWeight: FontWeight.w500, fontFamily: "Montserrat")),
+                    child: Text("• 2.5k View",
+                        style: TextStyle(
+                            color: warnaOren,
+                            fontSize: 10,
+                            fontWeight: FontWeight.w500,
+                            fontFamily: "Montserrat")),
                   ),
                 ],
               ),
-    
             ],
           ),
         ),
