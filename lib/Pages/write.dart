6d0f3037526b498ff49d26c46edfd62fc7e021b4
ChristianAbraham <christian.abraham.putra-2021@fst.unair.ac.id>
2023-02-17 09:16:17 +0700

    Flutter Quill added

--- lib/Pages/write.dart
+++ lib/Pages/write.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quill/flutter_quill.dart';
 
 class Write extends StatelessWidget {
   const Write({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Ini Write"),
-      ),
+    return Scaffold(
+      body: Column(
+      children: [
+        QuillToolbar.basic(controller: _controller),
+        Expanded(
+          child: Container(
+            child: QuillEditor.basic(
+              readOnly: false, controller: , // true for view only mode
+            ),
+          ),
+        )
+      ],
+)
     );
   }
 }
