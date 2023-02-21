60b1409484cb0974ee13af91ba22dad2e9227a5c
sakti17 <106225961+sakti17@users.noreply.github.com>
2023-02-21 10:28:13 +0700

    explore jadi

--- lib/Pages/explore.dart
+++ lib/Pages/explore.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:knowpedia/Components/searchbar.dart';
-import 'package:knowpedia/Pages/homepage.dart';
 
 const Color warnaUngu = Color.fromARGB(255, 75, 38, 72);
 const Color warnaOren = Color.fromARGB(255, 239, 140, 51);
@@ -36,7 +35,37 @@ class Explore extends StatelessWidget {
                 children: [
                   Padding(
                     padding: const EdgeInsets.only(bottom: 15),
-                    child: Container(width: 306, height: 149, color: warnaOren),
+                    child: Container(
+                      width: 306,
+                      height: 149,
+                      child: ClipRRect(
+                        borderRadius: BorderRadiusDirectional.circular(15),
+                        child: Stack(children: [
+                          const Image(
+                            width: 400,
+                            height: 200,
+                            fit: BoxFit.cover,
+                            image:
+                                NetworkImage("https://picsum.photos/300/220"),
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(left: 15, bottom: 15),
+                            child: Container(
+                              alignment: Alignment.bottomLeft,
+                              child: const Text(
+                                'Main Article',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w600,
+                                    fontFamily: 'Montserrat'),
+                              ),
+                            ),
+                          )
+                        ]),
+                      ),
+                    ),
                   ),
                   Row(
                     children: [
@@ -45,23 +74,61 @@ class Explore extends StatelessWidget {
                         child: Container(
                           width: 170,
                           height: 172,
-                          color: warnaUngu,
-                          child: Image(
-                              image: NetworkImage(
-                                  "https://picsum.photos/405/400")),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(15),
+                            child: Stack(children: [
+                              const Image(
+                                  fit: BoxFit.cover,
+                                  image: NetworkImage(
+                                      "https://picsum.photos/405/400")),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.only(bottom: 15, left: 15),
+                                child: Container(
+                                    alignment: Alignment.bottomLeft,
+                                    child: const Text(
+                                      'Technology',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.w600,
+                                          fontFamily: 'Montserrat'),
+                                    )),
+                              )
+                            ]),
+                          ),
                         ),
                       ),
                       Column(
                         children: [
-                          Container(
+                          SizedBox(
                             width: 177.3,
                             height: 78,
-                            color: warnaOren,
                             child: ClipRRect(
-                              child: Image(
-                                  fit: BoxFit.cover,
-                                  image: NetworkImage(
-                                      "https://picsum.photos/280/220")),
+                              borderRadius: BorderRadius.circular(15),
+                              child: Stack(children: [
+                                const Image(
+                                    width: 400,
+                                    height: 200,
+                                    fit: BoxFit.cover,
+                                    image: NetworkImage(
+                                        "https://picsum.photos/280/180")),
+                                Container(
+                                  alignment: Alignment.bottomLeft,
+                                  child: const Padding(
+                                    padding:
+                                        EdgeInsets.only(bottom: 15, left: 15),
+                                    child: Text(
+                                      'Sport',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.w600,
+                                          fontFamily: 'Montserrat'),
+                                    ),
+                                  ),
+                                )
+                              ]),
                             ),
                           ),
                           Padding(
@@ -69,81 +136,115 @@ class Explore extends StatelessWidget {
                             child: Container(
                               width: 177.3,
                               height: 78,
-                              color: warnaUngu,
-                              child: Image(
-                                  image: NetworkImage(
-                                      "https://picsum.photos/280/130")),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(15),
+                                child: Stack(children: [
+                                  const Image(
+                                      width: 400,
+                                      height: 200,
+                                      fit: BoxFit.cover,
+                                      image: NetworkImage(
+                                          "https://picsum.photos/280/180")),
+                                  Container(
+                                    alignment: Alignment.bottomLeft,
+                                    child: const Padding(
+                                      padding:
+                                          EdgeInsets.only(bottom: 15, left: 15),
+                                      child: Text(
+                                        'Food',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.w600,
+                                            fontFamily: 'Montserrat'),
+                                      ),
+                                    ),
+                                  )
+                                ]),
+                              ),
                             ),
                           ),
                         ],
                       ),
                     ],
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 15, bottom: 15),
-                    child: Container(
-                      width: 306,
-                      height: 200,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            height: 149,
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.circular(15),
-                              child: Image(
-                                  image: NetworkImage(
-                                      "https://picsum.photos/500/200")),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 7),
-                            child: Text("Lorem Ipsum Judul Waw",
-                                maxLines: 2,
-                                overflow: TextOverflow.ellipsis,
-                                style: TextStyle(
-                                    color: warnaUngu,
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w700,
-                                    fontFamily: "Montserrat")),
-                          ),
-                          Row(
-                            children: [
-                              Padding(
-                                padding: EdgeInsets.fromLTRB(0, 2, 8, 0),
-                                child: Text("• Science",
-                                    style: TextStyle(
-                                        color: warnaOren,
-                                        fontSize: 10,
-                                        fontWeight: FontWeight.w500,
-                                        fontFamily: "Montserrat")),
-                              ),
-                              Padding(
-                                padding: EdgeInsets.fromLTRB(0, 2, 8, 0),
-                                child: Text("• By Hana",
-                                    style: TextStyle(
-                                        color: warnaOren,
-                                        fontSize: 10,
-                                        fontWeight: FontWeight.w500,
-                                        fontFamily: "Montserrat")),
-                              ),
-                              Padding(
-                                padding: EdgeInsets.fromLTRB(0, 2, 8, 0),
-                                child: Text("• 2.5k View",
-                                    style: TextStyle(
-                                        color: warnaOren,
-                                        fontSize: 10,
-                                        fontWeight: FontWeight.w500,
-                                        fontFamily: "Montserrat")),
-                              ),
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
+                  const ExploreArticle(),
+                  const ExploreArticle(),
+                  const ExploreArticle(),
                 ],
               ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ExploreArticle extends StatelessWidget {
+  const ExploreArticle({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 15, bottom: 0),
+      child: SizedBox(
+        width: 306,
+        height: 200,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 149,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: const Image(
+                    image: NetworkImage("https://picsum.photos/500/200")),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 7, bottom: 5),
+              child: Text("Lorem Ipsum Judul Waw",
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                      color: warnaUngu,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w700,
+                      fontFamily: "Montserrat")),
+            ),
+            Row(
+              children: const [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 2, 8, 0),
+                  child: Text("• Science",
+                      style: TextStyle(
+                          color: warnaOren,
+                          fontSize: 10,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: "Montserrat")),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 2, 8, 0),
+                  child: Text("• By Hana",
+                      style: TextStyle(
+                          color: warnaOren,
+                          fontSize: 10,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: "Montserrat")),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 2, 8, 0),
+                  child: Text("• 2.5k View",
+                      style: TextStyle(
+                          color: warnaOren,
+                          fontSize: 10,
+                          fontWeight: FontWeight.w500,
+                          fontFamily: "Montserrat")),
+                ),
+              ],
             )
           ],
         ),
