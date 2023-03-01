4f68e9799e9dacd311166f5c36827c788933e610
sakti17 <106225961+sakti17@users.noreply.github.com>
2023-03-01 12:23:51 +0700

    update login

--- lib/Pages/login.dart
+++ lib/Pages/login.dart
@@ -1,108 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:knowpedia/Components/searchbar.dart';
+import 'package:knowpedia/main.dart';
 
-class LogIn extends StatelessWidget {
-  const LogIn({Key? key}) : super(key: key);
+void main(List<String> args) {
+  runApp(LoginScreen());
+}
 
-  static const String _title = 'Sample App';
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: _title,
-      home: Scaffold(
-        body: MyStatefulWidget(),
-      ),
-    );
-  }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  TextEditingController nameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView(
-          children: <Widget>[
-            Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.only(top: 350),
-                child: const Text(
-                  'KnowPedia',
-                  style: TextStyle(
-                      color: Colors.purple,
-                      fontWeight: FontWeight.w900,
-                      fontSize: 40),
-                )),
-            Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  '',
-                  style: TextStyle(fontSize: 20),
-                )),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                controller: nameController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Chris@mail.com',
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          resizeToAvoidBottomInset: false,
+          body: Stack(
+            children: [
+              Container(
+                height: 800,
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/images/LoginPage.png"),
+                        fit: BoxFit.cover)),
+              ),
+              Center(
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 500,
+                    ),
+                    Container(
+                      width: 325,
+                      child: TextField(
+                        textAlign: TextAlign.left,
+                        decoration: InputDecoration(
+                            fillColor: Colors.white,
+                            filled: true,
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(40),
+                                borderSide:
+                                    BorderSide(width: 1.5, color: warnaOren)),
+                            hintText: 'Chris@email.com',
+                            hintStyle: TextStyle(color: warnaOren)),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: Container(
+                        width: 325,
+                        child: TextField(
+                          textAlign: TextAlign.left,
+                          decoration: InputDecoration(
+                              fillColor: Colors.white,
+                              filled: true,
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(40),
+                                  borderSide:
+                                      BorderSide(width: 1.5, color: warnaOren)),
+                              hintText: 'Chris@email.com',
+                              hintStyle: TextStyle(color: warnaOren)),
+                        ),
+                      ),
+                    )
+                  ],
                 ),
               ),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-              child: TextField(
-                obscureText: true,
-                controller: passwordController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                ),
-              ),
-            ),
-            TextButton(
-              onPressed: () {
-                //forgot password screen
-              },
-              child: const Text('Forgot Password', textAlign: TextAlign.left),
-            ),
-            Container(
-                height: 50,
-                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: ElevatedButton(
-                  child: const Text('Login'),
-                  onPressed: () {
-                    print(nameController.text);
-                    print(passwordController.text);
-                  },
-                )),
-            Row(
-              children: <Widget>[
-                const Text('Does not have account?'),
-                TextButton(
-                  child: const Text(
-                    'Sign in',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  onPressed: () {
-                    //signup screen
-                  },
-                )
-              ],
-              mainAxisAlignment: MainAxisAlignment.center,
-            ),
-          ],
+            ],
+          ),
         ));
   }
 }
