68c6cdd04d2c9ac3b05435becd311d0255590173
ChristianAbraham <christian.abraham.putra-2021@fst.unair.ac.id>
2023-03-24 12:21:57 +0700

    clean code

--- lib/Components/searchbar.dart
+++ lib/Components/searchbar.dart
@@ -1,32 +1,28 @@
 import 'package:flutter/material.dart';
+import 'colors.dart';
 
-const Color warnaUngu = Color.fromARGB(255, 75, 38, 72);
-const Color warnaOren = Color.fromARGB(255, 239, 140, 51);
-
-Padding searchBar(){
+Padding searchBar() {
   return Padding(
-      padding: const EdgeInsets.only(bottom: 10),
-      child: Container(
-        height: 40,
-        color: Colors.transparent,
-        child: const TextField(
-          decoration: InputDecoration(
-            hintText: "Search",
-            hintStyle: TextStyle(
-                color: warnaUngu,
-                fontFamily: "Montserrat",
-                fontSize: 11.5),
-            prefixIcon: Icon(
-              Icons.search,
-              color: warnaUngu,
-              size: 18,
-            ),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.all(Radius.circular(50)),
-              borderSide: BorderSide(color: warnaUngu),
-            ),
+    padding: const EdgeInsets.only(bottom: 10),
+    child: Container(
+      height: 40,
+      color: Colors.transparent,
+      child: const TextField(
+        decoration: InputDecoration(
+          hintText: "Search",
+          hintStyle: TextStyle(
+              color: warnaUngu, fontFamily: "Montserrat", fontSize: 11.5),
+          prefixIcon: Icon(
+            Icons.search,
+            color: warnaUngu,
+            size: 18,
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(50)),
+            borderSide: BorderSide(color: warnaUngu),
           ),
         ),
       ),
-    );
+    ),
+  );
 }
