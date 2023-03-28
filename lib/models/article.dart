66cae55a75380d0a2b124649f14e60ccdefc125b
ChristianAbraham <christian.abraham.putra-2021@fst.unair.ac.id>
2023-03-28 14:37:42 +0700

    model added

---
+++ lib/models/article.dart
@@ -0,0 +1,17 @@
+class Article {
+  String title;
+  String description;
+  String image;
+  String author;
+  String category;
+  String content;
+
+  Article(
+    this.title,
+    this.description,
+    this.image,
+    this.author,
+    this.category,
+    this.content,
+  );
+}
