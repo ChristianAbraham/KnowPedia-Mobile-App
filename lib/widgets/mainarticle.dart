63bae1bd504929ed4caa92b8c91fd5b75f3e67bc
ChristianAbraham <christian.abraham.putra-2021@fst.unair.ac.id>
2023-04-03 14:36:31 +0700

    widget splited

--- lib/widgets/mainarticle.dart
+++ lib/widgets/mainarticle.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../Components/colors.dart';
 import '../Pages/articlepage.dart';
+import '../models/article.dart';
+import '../providers/articles.dart';
 
 class MainArticle extends StatelessWidget {
-  String image = "https://picsum.photos/280/130";
-  String title = "Lorem Ipsum Judul Waw";
-  String author = "Hana";
-
-  MainArticle({required this.image, required this.title, required this.author});
-
   @override
   Widget build(BuildContext context) {
+    final data = Provider.of<Article>(context);
+
     return InkWell(
       onTap: () {
-        Navigator.push(context,
-            MaterialPageRoute(builder: (context) => const ArticlePage()));
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => ArticlePage(data.title, data.image,
+                    data.author, data.category, data.content)));
       },
       child: Padding(
         padding: const EdgeInsets.only(right: 35),
@@ -29,58 +31,74 @@ class MainArticle extends StatelessWidget {
               ClipRRect(
                 borderRadius: BorderRadius.circular(15),
                 child: Image(
-                  image: NetworkImage(image),
+                  image: NetworkImage(data.image),
                   width: 280,
                   height: 130,
                   fit: BoxFit.cover,
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(2, 10, 0, 5),
-                child: SizedBox(
-                  width: 280,
-                  child: Text(title,
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      style: const TextStyle(
-                          color: warnaUngu,
-                          fontSize: 14,
-                          fontWeight: FontWeight.w700,
-                          fontFamily: "Montserrat")),
-                ),
-              ),
               Row(
-                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  const Padding(
-                    padding: EdgeInsets.fromLTRB(2, 2, 8, 0),
-                    child: Text("• Science",
-                        style: TextStyle(
-                            color: warnaOren,
-                            fontSize: 10,
-                            fontWeight: FontWeight.w500,
-                            fontFamily: "Montserrat")),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(0, 2, 8, 0),
-                    child: Text("• By $author",
-                        style: const TextStyle(
-                            color: warnaOren,
-                            fontSize: 10,
-                            fontWeight: FontWeight.w500,
-                            fontFamily: "Montserrat")),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.fromLTRB(0, 2, 8, 0),
-                    child: Text("• 2.5k View",
-                        style: TextStyle(
-                            color: warnaOren,
-                            fontSize: 10,
-                            fontWeight: FontWeight.w500,
-                            fontFamily: "Montserrat")),
-                  ),
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(2, 10, 0, 5),
+                          child: SizedBox(
+                            width: 220,
+                            child: Text(data.title,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: const TextStyle(
+                                    color: warnaUngu,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w700,
+                                    fontFamily: "Montserrat")),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            const Padding(
+                              padding: EdgeInsets.fromLTRB(2, 2, 8, 0),
+                              child: Text("• Science",
+                                  style: TextStyle(
+                                      color: warnaOren,
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.w500,
+                                      fontFamily: "Montserrat")),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.fromLTRB(0, 2, 8, 0),
+                              child: Text("• By ${data.author}",
+                                  style: const TextStyle(
+                                      color: warnaOren,
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.w500,
+                                      fontFamily: "Montserrat")),
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.fromLTRB(0, 2, 8, 0),
+                              child: Text("• 2.5k View",
+                                  style: TextStyle(
+                                      color: warnaOren,
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.w500,
+                                      fontFamily: "Montserrat")),
+                            ),
+                          ],
+                        ),
+                      ]),
+                  IconButton(
+                    splashRadius: 10,
+                    onPressed: () {},
+                    icon: Icon(Icons.favorite_border_rounded),
+                    iconSize: 20,
+                  )
                 ],
-              ),
+              )
             ],
           ),
         ),
--- lib/widgets/profilearticle.dart
+++ lib/widgets/profilearticle.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../providers/articles.dart';
+
 import '../Components/colors.dart';
 import '../Pages/articlepage.dart';
 import '../models/article.dart';
 
-class DailyInsight extends StatelessWidget {
+class ProfileArticle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final data = Provider.of<Article>(context);
-
     return InkWell(
       onTap: () {
         Navigator.push(
@@ -19,33 +18,33 @@ class DailyInsight extends StatelessWidget {
                     data.author, data.category, data.content)));
       },
       child: Padding(
-        padding: const EdgeInsets.only(bottom: 20, top: 5),
+        padding: const EdgeInsets.only(bottom: 15.0),
         child: Container(
-          width: 480,
-          height: 110,
-          color: Colors.transparent,
+          height: 120,
           child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: Image(
-                  image: NetworkImage(data.image),
-                  width: 100,
-                  height: 110,
-                  fit: BoxFit.cover,
+              SizedBox(
+                width: 120,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Image(
+                    image: NetworkImage(data.image),
+                    width: 280,
+                    height: 120,
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+                padding: const EdgeInsets.only(left: 10, bottom: 5),
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Padding(
                       padding: const EdgeInsets.only(top: 0, bottom: 1),
                       child: SizedBox(
-                        width: 250,
+                        width: 230,
                         child: Text(data.title,
                             maxLines: 2,
                             overflow: TextOverflow.ellipsis,
@@ -58,12 +57,11 @@ class DailyInsight extends StatelessWidget {
                       ),
                     ),
                     SizedBox(
-                      width: 250,
+                      width: 230,
                       child: Text(
                         data.description,
                         maxLines: 2,
                         overflow: TextOverflow.ellipsis,
-                        textAlign: TextAlign.justify,
                         style: const TextStyle(
                             color: warnaUngu,
                             height: 1.5,
@@ -73,36 +71,26 @@ class DailyInsight extends StatelessWidget {
                       ),
                     ),
                     Padding(
-                      padding: const EdgeInsets.only(top: 8),
+                      padding: const EdgeInsets.only(top: 10),
                       child: Row(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Padding(
-                            padding: const EdgeInsets.only(left: 0),
-                            child: Text("• ${data.category}",
-                                style: const TextStyle(
-                                    color: warnaOren,
-                                    fontSize: 10,
-                                    fontWeight: FontWeight.w500,
-                                    fontFamily: "Montserrat")),
+                          Container(
+                            width: 50,
+                            height: 20,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(5),
+                                color: warnaUngu),
+                            child: Center(
+                                child: Text(
+                              data.category,
+                              style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.w700,
+                                  fontFamily: "Montserrat"),
+                            )),
                           ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 5),
-                            child: Text("• By ${data.author}",
-                                style: const TextStyle(
-                                    color: warnaOren,
-                                    fontSize: 10,
-                                    fontWeight: FontWeight.w500,
-                                    fontFamily: "Montserrat")),
-                          ),
-                          // const Padding(
-                          //   padding: EdgeInsets.only(left: 130.0),
-                          //   child: Icon(
-                          //     Icons.favorite_border,
-                          //     color: warnaOren,
-                          //     size: 15,
-                          //   ),
-                          // ),
                         ],
                       ),
                     )
